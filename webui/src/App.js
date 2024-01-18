1e10190f44fe0ec94ef6bfa175b8132efc100d7e
Alan K Mathew <sill_mileage_04@icloud.com>
2024-01-18 23:19:50 +0100

    Created Initial Home Page

--- webui/src/App.js
+++ webui/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import MyJob from './components/MyJob.js';
+import AssistantAI from './components/AssistantAI.js';
+import AboutUs from './components/AboutUs.js';
+import Home from './components/Home.js';
+import Header from './components/Header.js';
+import UserPage from './components/UserPage.js';
+import Footer from './components/Footer.js';
 
-function App() {
+const App = () => {
+  const [selectedPage, setSelectedPage] = useState('Home');
+
+  const handleSelection = (page) => {
+    setSelectedPage(page);
+  };
+
+  const renderPage = () => {
+    switch (selectedPage) {
+      case 'MyJob':
+        return <MyJob />;
+      case 'AssistantAI':
+        return <AssistantAI />;
+      case 'AboutUs':
+        return <AboutUs />;
+      case 'UserPage':
+        return <UserPage />;
+      default:
+        return <Home />;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <Header onSelection={handleSelection} currentPage={selectedPage} />
+      <div className="page-content">
+        {renderPage()}
+      </div>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
