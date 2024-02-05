99f7c037d6122bed1275369e980109766ee75bbc
MaheshNidugala97 <133670096+MaheshNidugala97@users.noreply.github.com>
2024-02-05 23:35:27 +0100

    Heroku deploy

--- middleware/google.auth.js
+++ middleware/google.auth.js
@@ -1,6 +1,4 @@
 const { OAuth2Client } = require('google-auth-library');
-const { getClientIdentityTokens } = require('../services/auth.services');
-const { logger } = require('../middleware/logging');
 
 const oAuth2Client = new OAuth2Client({
     clientId: process.env.GOOGLE_CLIENT_ID,
