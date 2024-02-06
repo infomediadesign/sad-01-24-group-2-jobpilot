3b336f918ad9e96301e896904535b41da6c5ea08
MaheshNidugala97 <133670096+MaheshNidugala97@users.noreply.github.com>
2024-02-06 23:47:37 +0100

    Fix access token logging in auth.router.js

--- Backend/online-auth-service/routes/auth.router.js
+++ Backend/online-auth-service/routes/auth.router.js
@@ -196,8 +196,8 @@ router.get('/google/callback', async (req, res) => {
             secure: true,
             domain: 'jobpilot-fb225ee580d2.herokuapp.com',
         });
-        await res.redirect('https://jobpilot-fb225ee580d2.herokuapp.com/dashboard');
-        logger.info('Authentication successful!');
+        res.send('Authentication successful!');
+        // await res.redirect('https://jobpilot-fb225ee580d2.herokuapp.com/dashboard');
     } catch (err) {
         logger.error(err);
         return res.status(500).json({
