175d47dec02b7145ab1cf747e77ddf5bd62e2ca3
Alan K Mathew <sill_mileage_04@icloud.com>
2024-02-08 00:55:51 +0100

    Update team member avatars

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -11,7 +11,7 @@ const About = () => {
       <h2>Our Team</h2>
       <div className="team-section">
         <div className="team-card">
-          <img src="https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg" alt="Team member 1" />
+          <img src="https://i.ibb.co/qs0Zwc0/2-A1-DEA05-9292-497-C-8-ACD-1-E11-AB6393-EA.jpg" alt="Team member 1" />
           <div className="container">
             <h2>Adith Adithya</h2>
             <p className="titleAboutUs">Frontend Developer</p>
@@ -19,7 +19,7 @@ const About = () => {
         </div>
 
         <div className="team-card">
-          <img src="https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg" alt="Team member 2" />
+          <img src="https://i.ibb.co/Jj8ZLML/B5-E4718-F-2618-44-FF-8114-8-E34-F6-B41727-1-201-a.jpg" alt="Team member 2" />
           <div className="container">
             <h2>Alan K Mathew</h2>
             <p className="titleAboutUs">Frontend Developer</p>
@@ -27,7 +27,7 @@ const About = () => {
         </div>
 
         <div className="team-card">
-          <img src="https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg" alt="Team member 3" />
+          <img src="https://i.ibb.co/GWjh9ky/Mahesh-Professional-Photo-Copy.jpg" alt="Team member 3" />
           <div className="container">
             <h2> Mahesh Nidugala</h2>
             <p className="titleAboutUs">Backend Developer</p>
